2bab1547a311aae046ba3abd00552179f40f43f4
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2023-04-27 23:53:25 +0300

    added possibility to override the default cache

--- Sources/domain/ImageLoader.swift
+++ Sources/domain/ImageLoader.swift
@@ -10,13 +10,17 @@ import Foundation
 /// Object responsible to load a `GIFSource` into a `ImageFrame` stream.
 public struct ImageLoader {
     public let session: URLSession
-    public let cache: URLCache
+    public private(set) var cache: URLCache
     public let fileManager: FileManager
 
     public init(session: URLSession = .shared, cache: URLCache = .shared, fileManager: FileManager = .default) {
         self.session = session
         self.cache = cache
         self.fileManager = fileManager
+    }
+    
+    public func set(cache: URLCache) {
+        self.cache = cache
     }
 
     public func load(source: GIFSource) async throws -> CGImageSourceFrameSequence {
